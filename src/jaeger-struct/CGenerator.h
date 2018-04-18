d3df3e4927c2fa86f24561187dca274f038dc8ee
Isaac Hier <ihier@uber.com>
2018-04-18 12:57:14 -0400

    Fix compilation and begin with example file

--- src/jaeger-struct/CGenerator.h
+++ src/jaeger-struct/CGenerator.h
@@ -1,12 +1,17 @@
 #ifndef JAEGER_STRUCT_C_GENERATOR
 #define JAEGER_STRUCT_C_GENERATOR
 
-#include <protobuf/code_generator.h>
+#include <google/protobuf/compiler/code_generator.h>
 
 namespace jaegertracing {
 namespace structgen {
 
-class CGenerator : public google::protobuf::CodeGenerator {
+class CGenerator : public google::protobuf::compiler::CodeGenerator {
+  public:
+    bool Generate(const google::protobuf::FileDescriptor* file,
+                  const std::string& parameter,
+                  google::protobuf::compiler::GeneratorContext* context,
+                  std::string* error) const override;
 };
 
 }  // namespace structgen
